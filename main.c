a0955a653249bed671b1b4f206ddd09bcea2fac7
Kilian Pouliquen <kipouliq@bess-f2r5s10.clusters.42paris.fr>
2024-01-12 12:29:06 +0100

    added free to gnl

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "./megalibft.h"
-#include <stdio.h>
-int main()
-{
-    int fd = open("./Makefile", O_RDONLY);
-    char *str = get_next_line(fd, 1);
-    printf("%s", str);
-    free(str);
-}
